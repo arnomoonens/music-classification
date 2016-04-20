02523b8cd2ff876160577032b887b5f7494b9466
Arno Moonens <x-006@hotmail.com>
2016-04-20 16:02:22 +0200

    Added crossvalidate script and made classification code compatible with it

--- classification.py
+++ classification.py
@@ -1,21 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
-import pandas
+import pandas as pd
 import random
 import itertools
 from collections import Counter
 
+column_names = ['Id', 'Performer', 'Title', 'Inst.', 'Style', 'Year', 'Tempo', 'Number of Notes']
+output_columns = ['Performer', 'Inst.', 'Style', 'Year', 'Tempo']
+
 # Random with probability
-def learn(ngram_folder, descriptions):
-    df = pandas.read_csv(descriptions, sep=';', index_col=0)
-    frequencies = {x: Counter(df[x]) for x in ['Performer', 'Inst.', 'Style', 'Year']}
+def learn(input_data_file):
+    df = pd.read_csv(input_data_file, sep=';', index_col=0, names=column_names)
+    frequencies = {x: Counter(df[x]) for x in output_columns}
     return lambda x: {k: next(itertools.islice(frequencies[k].elements(), random.randrange(sum(frequencies[k].values())), None)) for k in frequencies}
 
-
+#Output order: Performer, Instrument, Style, Year, Tempo
+def test_classifier(classifier, test_data_file):
+    df = pd.read_csv(test_data_file, sep=';', index_col=0, names=column_names)
+    output_df = pd.DataFrame(columns=output_columns)
+    for song in df.iterrows():
+        output_df=output_df.append(classifier(song), ignore_index=True)
+    return output_df
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print("Please provide a folder with the n-grams, descriptions file and number of song to classify")
+        print('Please provide a training input csv file, test input csv file and output file.')
     else:
-        classifier = learn(sys.argv[1], sys.argv[2])
-        print(classifier(sys.argv[3]))
+        training_data_file = sys.argv[1]
+        test_data_file = sys.argv[2]
+        output_file = sys.argv[3]
+        classifier = learn(training_data_file)
+        results = test_classifier(classifier, test_data_file)
+        results.to_csv(output_file, header=False, sep=';', index=False)
