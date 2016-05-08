1861161d44261b7b86ceabc3125bb2eacaa63881
JensNevens <jens.nevens@gmail.com>
2016-05-08 16:51:55 +0200

    Added parameter for N (N-gram size) and profile_size
    Added notebook for testing purposes.

--- classification.py
+++ classification.py
@@ -20,8 +20,8 @@ def learn_random(input_data_file):
     return lambda x: {k: next(itertools.islice(frequencies[k].elements(), random.randrange(sum(frequencies[k].values())), None)) for k in frequencies}
 
 # Generate a profile using song data
-def get_profile(song_df):
-    ngram = generate_ngram(song_df, 3, '--both')
+def get_profile(song_df, N):
+    ngram = generate_ngram(song_df, N, '--both')
     c = Counter()
     for x in ngram:
         c[tuple([float(nr) for nr in x])] += 1
@@ -37,10 +37,10 @@ def similarity(type_profile, song_profile):
     return sum([4 - ((2 * (new_type_profile[k] - new_song_profile[k])) / (new_type_profile[k] + new_song_profile[k])) ** 2 for k in new_type_profile.keys()])  # Similarity formula from original paper
 
 # Make a classifier based on ngram profiles
-def learn_with_profiles(input_data_file):
+def learn_with_profiles(input_data_file, N, profile_size):
     df = pd.read_csv(input_data_file, sep=';', index_col=0, names=column_names)
     logging.info('Making profiles for songs')
-    df['profile'] = df.apply(lambda row: get_profile(pd.read_csv("unigram/" + str(row.name) + ".csv", index_col=0)), axis=1)  # Make profile for each song in input
+    df['profile'] = df.apply(lambda row: get_profile(pd.read_csv("unigram/" + str(row.name) + ".csv", index_col=0), N), axis=1)  # Make profile for each song in input
     logging.info('Profiles for songs made')
     logging.info('Making profiles for types')
     type_profiles = pd.DataFrame(columns=['type', 'name', 'profile'])
@@ -48,11 +48,14 @@ def learn_with_profiles(input_data_file):
         grouped = df.groupby(output_column)  # …group the rows by that output type…
         for name, group in grouped:
             profile = group.loc[:, 'profile'].sum()  # …and build a profile for each instance of a type (e.g. profile of a specific performer) by summing the profiles of the songs with that specific output type
+            flat_profile = list(profile.items())
+            flat_profile = flat_profile[:profile_size] # limit profile to profile_size
+            profile = Counter(dict(flat_profile))
             type_profiles = type_profiles.append({'type': output_column, 'name': name, 'profile': profile}, ignore_index=True)
     logging.info('Profiles for types made')
     types_grouped = type_profiles.groupby('type')  # Group of all performers, group of all years,…
     # classifier checks for each value's profile of output_column which is most similar to the song's profile (by an argmax in each group of types_grouped)
-    return lambda song_df: {t: group.loc[np.argmax(group.apply(lambda row: similarity(row['profile'], get_profile(song_df)), axis=1))]['name'] for t, group in types_grouped}
+    return lambda song_df: {t: group.loc[np.argmax(group.apply(lambda row: similarity(row['profile'], get_profile(song_df, N)), axis=1))]['name'] for t, group in types_grouped}
 
 
 # Apply a classifier on test data and output the results
@@ -77,7 +80,7 @@ if __name__ == '__main__':
         training_data_file = sys.argv[1]
         test_data_file = sys.argv[2]
         output_file = sys.argv[3]
-        classifier = learn_with_profiles(training_data_file)
+        classifier = learn_with_profiles(training_data_file, 3, 1000)
         logging.info('Classifier made, now testing it.')
         results = test_classifier(classifier, test_data_file)
         logging.info('Test results gathered, writing them to file')
